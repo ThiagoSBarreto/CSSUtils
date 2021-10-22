3a95d3d95841870ded34bbc73b2f8b92ef1c0579
Thiago da Silva Barreto <thiagobarretocbv@hotmail.com>
2021-10-22 16:10:04 -0300

    Adicionado Documentação do ModelFinder

--- CSSUtils/ViewModelResolver/IModelFinder.cs
+++ CSSUtils/ViewModelResolver/IModelFinder.cs
@@ -8,6 +8,13 @@ namespace CSSUtils.ViewModelResolver
 {
     public interface IModelFinder
     {
+        /// <summary>
+        /// <para>Encontra, Instancia e Coloca o ViewModel apropriado para esta classe como o DataContext</para>
+        /// <para>Deve seguir a seguinte regra: View: PageView / ViewModel: PageViewModel</para>
+        /// <para>O ViewModel deve conter o metodo Dispose que será chamado automaticamente</para>
+        /// <para>quando a Window ou Page é descarregado, o metodo Dispose é chamado automaticamente</para>
+        /// </summary>
+        /// <param name="View">"this" a Instancia atual da Classe da View</param>
         void Resolve(object View);
     }
 }
